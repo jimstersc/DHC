6f9fe56c206f851d7653550cf8b5077a8467a870
jimstersc <jim.lathan@gmail.com>
2023-06-29 22:17:22 -0400

    Create rdp_fix.sh

---
+++ rdp_fix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Make Directory and Extract Files
+mkdir rdp_fix
+tar -xvf MultiMonitor.tgz -C rdp_fix
+
+#Copy files to /usr/bin
+cd rdp_fix
+cp dhc-apply-config.pyc /usr/bin
+cp dhc_apply_config.so /usr/bin
