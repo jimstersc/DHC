4e2279939472790eff8becc9356061838d5f8781
jimstersc <jim.lathan@gmail.com>
2023-06-29 23:41:25 -0400

    Update rdp_fix.sh

--- rdp_fix.sh
+++ rdp_fix.sh
@@ -7,3 +7,7 @@ tar -xvf RDP_MultiMonitor.tgz -C rdp_fix
 cd rdp_fix
 cp dhc-apply-config.pyc /usr/bin
 cp dhc_apply_config.so /usr/bin
+cd DHC-main
+bash rdp_fox.sh
+This will copy files to fix RDP in multi monitor sessions.
+exit and reboot
